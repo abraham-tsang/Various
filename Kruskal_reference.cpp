8fd5823917bce8ae2c8beeac55e6306c957e21e8
abraham-tsang <abraham.tsang@gmail.com>
2020-08-13 08:36:33 -0400

    finally completed kruskal

--- Kruskal_reference.cpp
+++ Kruskal_reference.cpp
@@ -1,120 +1,125 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 
-class Edge{
-public:
-    int src, dest, weight;
-};
+std::vector<std::vector<std::pair<int, int>>> graph;
+std::vector<std::vector<std::pair<int, int>>> mst;
+std::vector<bool> visited;
+std::vector<bool> recStack;
+int v = 4;
 
-class Subset{
-public:
-    int parent, rank;
-};
+void addEdge(int u, int v, int weight){
+    graph[u].push_back(std::make_pair(v, weight));
+}
 
-int partition(Edge * edge, int low, int high){
+std::pair<int, std::pair<int, int>> findSmallestEdge(){
 
-    int pivot = edge[high].weight;
-    int i = low - 1;
-    Edge temp;
-
-    for(int j = low; j <= high - 1; j++){
-	if(edge[j].weight < pivot){
-	    i++;
-	    temp = edge[i];
-	    edge[i] = edge[j];
-	    edge[j] = temp;
+    int u, v, min = 99999;
+    for(int i = 0; i < v; i++){
+	for(int j = 0; j < graph[i].size(); j++){
+	    if(graph[i][j].second < min){
+		min = graph[i][j].second;
+		u = i;
+		v = graph[i][j].first;
+	    }
 	}
     }
-    temp = edge[i + 1];
-    edge[i + 1] = edge[high];
-    edge[high] = temp;
-    return i + 1;
 
+    return std::make_pair(u, std::make_pair(v, min));
 }
 
-void quickSort(Edge * edge, int low, int high){
-    if(low < high){
-	int pi = partition(edge, low, high);
-
-	quickSort(edge, low, pi - 1);
-	quickSort(edge, pi + 1, high);
+bool DFSUtil(int u){
+    if(!visited[u]){
+	visited[u] = true;
+	recStack[u] = true;
+	for(int i = 0; i < mst[u].size(); i++){
+		std::cout << mst[u][i].second << std::endl;
+	    if(!visited[mst[u][i].first] && DFSUtil(mst[u][i].first)){
+	        return true;
+	    }
+	    else if(recStack[mst[u][i].first]){
+	        return true;
+	    }
+        }
     }
+    recStack[u] = false;
+    return false;
 
 }
+bool cycleFormed(){
 
-int find(Subset subset[], int i){
-
-    if(subset[i].parent != i){
-	subset[i].parent = find(subset, subset[i].parent);
+    for(int i = 0; i < v; i++){
+	if(DFSUtil(i)){
+            return true;
+	}
     }
-    return subset[i].parent;
+    return false;
+}
 
+
+void includeEdge(std::pair<int, std::pair<int, int>> edge){
+    mst[edge.first].push_back(std::make_pair(edge.second.first, edge.second.second));
+}
+
+void deleteEdge(int which, std::pair<int, std::pair<int, int>> edge){
+
+    if(which == 0){
+	for(int i = 0; i < graph[edge.first].size(); i++){
+	    if(graph[edge.first][i].first == edge.second.first){
+	        graph[edge.first].erase(graph[edge.first].begin() + i);
+	    }
+        }
+    }
+    else{
+        for(int i = 0; i < mst[edge.first].size(); i++){
+	    if(mst[edge.first][i].first == edge.second.first){
+	        mst[edge.first].erase(mst[edge.first].begin() + i);
+	    }
+        }
+    }
 }
 
 int main(){
 
-    int v = 4;
-    int e = 5;
-    Edge * edge = new Edge[e];
-
-    edge[0].src = 0;
-    edge[0].dest = 1;
-    edge[0].weight = 10;
-
-    edge[1].src = 0;
-    edge[1].dest = 2;
-    edge[1].weight = 6;
-
-    edge[2].src = 0;
-    edge[2].dest = 3;
-    edge[2].weight = 5;
-
-    edge[3].src = 1;
-    edge[3].dest = 3;
-    edge[3].weight = 15;
-
-    edge[4].src = 2;
-    edge[4].dest = 3;
-    edge[4].weight = 4;
-
-    Edge * result = new Edge[v];
-
-    quickSort(edge, 0, e - 1);
-    
-    Subset * subset = new Subset[v];
+    std::vector<std::pair<int, int>> temp;
     for(int i = 0; i < v; i++){
-	subset[i].parent = i;
-	subset[i].rank = 0;
+	graph.push_back(temp);
     }
 
-    int a = 0;
-    int b = 0;
+    addEdge(0, 1, 10);
+    addEdge(0, 2, 6);
+    addEdge(3, 0, 5);
+    addEdge(1, 3, 15);
+    addEdge(2, 3, 4);
+    
+    for(int i = 0; i < v; i++){
+	visited.push_back(false);
+	recStack.push_back(false);
+    }
+    
+    for(int i = 0; i < v; i++){
+	mst.push_back(temp);
+    }
 
-    while(a < v - 1 && b < e){
-	Edge next_edge = edge[b];
-	b++;
-	int x = find(subset, next_edge.src);
-	int y = find(subset, next_edge.dest);
-
-	if(x != y){
-	    result[a] = next_edge;
-	    a++;
-	    int xroot = find(subset, x);
-	    int yroot = find(subset, y);
-
-	    if(subset[xroot].rank < subset[yroot].rank){
-		subset[xroot].parent = yroot;
-	    }
-	    else if(subset[xroot].rank > subset[yroot].rank){
-		subset[yroot].parent = xroot;
-	    }
-	    else{
-		subset[yroot].parent = xroot;
-		subset[xroot].rank++;
-	    }
+    int it = 0;
+    
+    while(it != v - 1){
+	std::pair<int, std::pair<int, int>> edge = findSmallestEdge();
+	includeEdge(edge);
+	if(!cycleFormed()){
+	    it++;
 	}
-	
+	else{
+	    deleteEdge(1, edge);
+	}
+	deleteEdge(0, edge);
     }
-    for(int i = 0; i < a; i++){
-	std::cout << result[i].src << " " << result[i].dest << " " << result[i].weight << std::endl;
+
+    for(int i = 0; i < v; i++){
+	for(int j = 0; j < mst[i].size(); j++){
+	    std::cout << i << " " << mst[i][j].first << " " << mst[i][j].second << std::endl;
+	}
+	std::cout << std::endl;
     }
+
 }
