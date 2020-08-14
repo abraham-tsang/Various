db1c12d835a7b4db43e3c1cb2f93befa73d5124f
abraham-tsang <abraham.tsang@gmail.com>
2020-08-14 12:58:40 -0400

    replaced old johnson with new johnson

--- Johnson_reference.cpp
+++ Johnson_reference.cpp
@@ -1,102 +1,113 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <climits>
 
-class Edge{
-public:
-    int src, dest, weight;
-};
+std::vector<std::vector<std::pair<int, int>>> graph;
+
+void addEdge(int u, int v, int weight){
+    graph[u].push_back(std::make_pair(v, weight));
+}
 
 int main(){
 
     int v = 5;
-    int e = 9;
-    Edge * edge = new Edge[e];
+    int e = 8;
+    std::vector<std::pair<int, int>> temp;
+    for(int i = 0; i < v; i++){
+	graph.push_back(temp);
+    }
 
-    edge[0].src = 0;
-    edge[0].dest = 1;
-    edge[0].weight = -5;
-
-    edge[1].src = 0;
-    edge[1].dest = 2;
-    edge[1].weight = 2;
-    
-    edge[2].src = 0;
-    edge[2].dest = 3;
-    edge[2].weight = 3;
-    
-    edge[3].src = 1;
-    edge[3].dest = 2;
-    edge[3].weight = 4;
-    
-    edge[4].src = 2;
-    edge[4].dest = 3;
-    edge[4].weight = 1;
-    
-    edge[5].src = 4;
-    edge[5].dest = 0;
-    edge[5].weight = 0;
-    
-    edge[6].src = 4;
-    edge[6].dest = 1;
-    edge[6].weight = 0;
-    
-    edge[7].src = 4;
-    edge[7].dest = 2;
-    edge[7].weight = 0;
-    
-    edge[8].src = 4;
-    edge[8].dest = 3;
-    edge[8].weight = 0;
+    addEdge(0, 1, -5);
+    addEdge(0, 2, 2);
+    addEdge(0, 3, 3);
+    addEdge(1, 2, 4);
+    addEdge(2, 3, 1);
+    addEdge(4, 0, 0);
+    addEdge(4, 1, 0);
+    addEdge(4, 2, 0);
+    addEdge(4, 3, 0);
 
     int dist[v];
-
     for(int i = 0; i < v; i++){
 	dist[i] = INT_MAX;
     }
-    dist[v-1] = 0;
-    
+    dist[v - 1] = 0;
+
     for(int i = 0; i < v - 1; i++){
-	for(int j = 0; j < e; j++){
-	    int u = edge[j].src;
-	    int v = edge[j].dest;
-	    int weight = edge[j].weight;
+	for(int j = 0; j < graph.size(); j++){
+	    for(int k = 0; k < graph[j].size(); k++){
+		int u = j;
+		int v = graph[j][k].first;
+		int weight = graph[j][k].second;
+		if(dist[u] != INT_MAX && dist[u] + weight < dist[v]){
+		    dist[v] = dist[u] + weight;
+		}
+	    }
+	}
+    }
+
+    for(int i = 0; i < graph.size(); i++){
+	for(int j = 0; j < graph[i].size(); j++){
+	    int u = i;
+	    int v = graph[i][j].first;
+	    int weight = graph[i][j].second;
 	    if(dist[u] != INT_MAX && dist[v] > dist[u] + weight){
-		dist[v] = dist[u] + weight;
+		std::cout << "Graph contains negative weight cycle";
 	    }
 	}
     }
 
-    for(int i = 0; i < e; i++){
-	int u = edge[i].src;
-	int v = edge[i].dest;
-	int weight = edge[i].weight;
-	if(dist[u] != INT_MAX && dist[v] > dist[u] + weight){
-	    std::cout << "Graph contains negative weight cycle";
+    for(int i = 0; i < graph.size(); i++){
+	for(int j = 0; j < graph[i].size(); j++){
+	    int u = i;
+	    int v = graph[i][j].first;
+	    int weight = graph[i][j].second;
+	    graph[i][j].second = weight + dist[u] - dist[v];
 	}
     }
 
-    for(int i = 0; i < e; i++){
-	int u = edge[i].src;
-	int v = edge[i].dest;
-	int weight = edge[i].weight;
-	edge[i].weight = weight + dist[u] - dist[v];
-	//std::cout << u << " " << v << " " << edge[i].weight << std::endl;
+    graph.erase(graph.begin() + v - 1);
+    v--;
+    std::vector<std::vector<int>> distance;
+    std::vector<int> temp2;
+    std::vector<bool> sptSet;
+    for(int i = 0; i < v; i++){
+	distance.push_back(temp2);
+	sptSet.push_back(false);
     }
 
-    for(int i = 0; i < e; i++){
-	if(edge[i].weight == 0){
-	    for(int j = i; j < e; j++){
-	        edge[j] = edge[j + 1];
+    for(int i = 0; i < graph.size(); i++){
+	for(int j = 0; j < v; j++){
+            distance[i].push_back(INT_MAX);
+	    sptSet[j] = false;
+	}
+	distance[i][i] = 0;
+	for(int j = 0; j < v - 1; j++){
+	    int min = INT_MAX;
+	    int u;
+	    for(int k = 0; k < v; k++){
+		if(sptSet[k] == false && distance[i][j] <= min){
+		    min = distance[i][j];
+		    u = j;
+		}
+	    }
+	    sptSet[u] = true;
+
+	    for(int k = 0; k < v; k++){
+		for(int l = 0; l < k; l++){
+		    if(!sptSet[k] && graph[u][l].first == k && distance[i][u] != INT_MAX && distance[i][u] + graph[u][l].second < distance[i][k]){
+			distance[i][k] = distance[i][u] + graph[u][l].second;
+		    }
+		}
 	    }
 	}
-	//std::cout << edge[i].weight << std::endl;
     }
-
-
-
-    /*for(int i = 0; i < v - 1; i++){
-	std::cout << i << " " << dist[i] << std::endl;
-    }*/
+    
+    for(int i = 0; i < v; i++){
+	for(int j = 0; j < distance[i].size(); j++){
+	    std::cout << i << " " << j << " " << distance[i][j] << std::endl;
+	}
+    }
 
 }
