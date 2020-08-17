749e9d3617be3b456fc853de1ae126523210fe31
abraham-tsang <abraham.tsang@gmail.com>
2020-08-17 18:14:55 -0400

    trained DFS

---
+++ DFS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> graph;
+std::vector<int> visited;
+
+void addEdge(int u, int v){
+    graph[u].push_back(v);
+}
+
+void DFSUtil(int s){
+    visited[s] = true;
+    std::cout << s << " ";
+    for(int i = 0; i < graph[s].size(); i++){
+	if(!visited[graph[s][i]]){
+	    DFSUtil(graph[s][i]);
+	}
+    }
+}
+
+int main(){
+
+    int v = 4;
+
+    std::vector<int> temp;
+    for(int i = 0; i < v; i++){
+	graph.push_back(temp);
+    }
+
+    addEdge(0, 1);
+    addEdge(0, 2);
+    addEdge(1, 2);
+    addEdge(2, 0);
+    addEdge(2, 3);
+    addEdge(3, 3);
+
+    for(int i = 0; i < v; i++){
+	visited.push_back(false);
+    }
+
+    int s = 2;
+
+    DFSUtil(s);
+
+}
